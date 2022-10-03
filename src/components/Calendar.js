7517fdd6c54aee55661777120e0815b76005cbcc
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-10-03 14:49:26 +0200

    Added submit and change handlers to CalendarForm

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import CalendarForm from "./CalendarForm";
 
-class Calender extends React.Component{
-    state={
-        meetings: []
-    }
+class Calender extends React.Component {
+  state = {
+    meetings: [],
+  };
+  render() {
+   return <>
+      <CalendarForm></CalendarForm>
+    </>;
+  }
 }
-export default Calender
+export default Calender;
--- src/components/CalendarForm.js
+++ src/components/CalendarForm.js
@@ -1,25 +1,80 @@
 import React from "react";
 class CalendarForm extends React.Component {
   state = {
-    value: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    date: "",
+    time: "",
   };
   render() {
+    const { firstName, lastName, email, date, time } = this.state;
+    console.log(this.state);
     return (
-      <form className="panel__form">
+      <form className="panel__form" onSubmit={this.handleSubmit}>
         <label>
-          First name: <input type={"text"} name="firstname"></input>
+          First name:{" "}
+          <input
+            onChange={this.handleChange}
+            value={firstName}
+            type="text"
+            name="firstName"
+            required
+          ></input>
         </label>
         <label>
-          Last name: <input type={"text"} name="lastname"></input>{" "}
+          Last name:{" "}
+          <input
+            onChange={this.handleChange}
+            value={lastName}
+            type="text"
+            name="lastName"
+            required
+          ></input>{" "}
         </label>
         <label>
-          Last name: <input type={"email"} name="lastname"></input>{" "}
+          Email:{" "}
+          <input
+            onChange={this.handleChange}
+            value={email}
+            type="temail"
+            name="email"
+            required
+          ></input>{" "}
         </label>
         <label>
-            <input type={date}></input>
+          Date:{" "}
+          <input
+            onChange={this.handleChange}
+            value={date}
+            type="date"
+            name="date"
+            required
+          ></input>{" "}
         </label>
+        <label>
+          Time:{" "}
+          <input
+            onChange={this.handleChange}
+            value={time}
+            type="time"
+            name="time"
+            required
+          ></input>{" "}
+        </label>
+        <input type="submit" value="Add meeting!" />
       </form>
     );
   }
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({
+      ...this.state,
+      [e.target.name]: value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
 }
 export default CalendarForm;
